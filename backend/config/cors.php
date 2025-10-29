641514ad1ac57b2d316147b1bc4125984bd645fb
Eduardo Stein <dudustein2024@gmail.com>
2025-10-29 12:18:14 -0300

    Correção de cors

---
+++ backend/config/cors.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Determine what cross-origin operations may execute in web browsers. You
+    | can adjust the allowed origins, methods, and headers as required by
+    | your frontends. Update the origins list when deploying elsewhere.
+    |
+    */
+
+    'paths' => ['api/*'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://applldespachante.skalacode.com',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+];
