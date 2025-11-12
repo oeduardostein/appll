2a36e4c1a1a3cb50e80034f7a4658417b918e970
Eduardo Stein <dudustein2024@gmail.com>
2025-11-12 10:41:19 -0300

    Correções da play store 2

--- frontend-app/android/app/src/main/kotlin/br/com/appgestaoll/MainActivity.kt
+++ frontend-app/android/app/src/main/kotlin/br/com/appgestaoll/MainActivity.kt
@@ -1,12 +1,21 @@
 package br.com.appgestaoll
 
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).apply {
+            isAppearanceLightStatusBars = true
+            isAppearanceLightNavigationBars = true
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_BARS_BY_SWIPE
+            }
+        }
     }
 }
